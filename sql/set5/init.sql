08db92db895316deef82256158603dc8c15df0ba
lalyos <lalyos@yahoo.com>
2020-10-16 12:58:51 +0200

    create 2 sql sets

---
+++ sql/set5/init.sql
@@ -0,0 +1,8 @@
+use mysql;
+create TABLE person (name VARCHAR(20));
+
+insert into person VALUES ('trump');
+insert into person VALUES ('boris');
+insert into person VALUES ('victor');
+insert into person VALUES ('angela');
+insert into person VALUES ('vladimir');
